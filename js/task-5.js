b365e074ac364894cec0024fe4c6aaa1ab27dd0e
Julia2906 <137899526+Julia2906@users.noreply.github.com>
2025-01-16 18:54:03 +0200

    task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnEl = document.querySelector('.change-color');
+const bodyColorEl = document.querySelector('body');
+const textColorEl = document.querySelector('.color');
+
+btnEl.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  bodyColorEl.style.backgroundColor = randomColor;
+  textColorEl.textContent = randomColor;
+});
