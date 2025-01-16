90014737ef8290040e0e5fa4c85a294430f460a5
Julia2906 <137899526+Julia2906@users.noreply.github.com>
2025-01-16 18:20:15 +0200

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const emailValue = formEl.elements.email.value.trim();
+  const passwordValue = formEl.elements.password.value.trim();
+
+  if (emailValue === '' || passwordValue === '') {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formData = {
+    [formEl.elements.email.name]: emailValue,
+    [formEl.elements.password.name]: passwordValue,
+  };
+  console.log(formData);
+
+  formEl.reset();
+});
